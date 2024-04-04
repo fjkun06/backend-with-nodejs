25b541c2af72aab434e1fb17825b2171ff9905b4
= <=>
2024-04-04 12:52:28 +0100

    Logging to file with Morgan

--- movie-db/index.js
+++ movie-db/index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { movieRouter } from "./movie/index.js";
+import morgan from "morgan";
+import { createWriteStream } from "fs";
 
 const app = express();
+//logging to file
+const accessLogStream = createWriteStream("access.log", { flags: "a" });
 
-function log(req, res, next) {
-  console.log(req.url);
-  next();
-}
-
-app.use(log)
+app.use(morgan("common", { immediate: true, stream: accessLogStream }));
 app.use("/movie", movieRouter);
 app.get("/", (req, res) => res.redirect("/movie"));
 // app.get("/", (req, res) => res.send("Movie Database App"));
